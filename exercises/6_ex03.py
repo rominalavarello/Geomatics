4da1e9645691f32fbb5554a520135b2bea95b293
rominalavarello <rominagianella.lavarelloschettini@student.unibz.it>
2024-06-07 16:12:33 +0200

    CREATE A GPKG WITH THE COUNTRIES CENTROIDS

---
+++ exercises/6_ex03.py
@@ -0,0 +1,62 @@
+# exercise 03
+from pyqgis_scripting_ext.core import *
+
+folder = "/Users/rominalavarello/Desktop/Geomatics/"
+gpkgPATH = folder + "miniGPKG.gpkg"
+
+HMap.remove_layers_by_name(["centroids",countriesName,"OpenStreetMap"])
+
+osm = HMap.get_osm_layer()
+HMap.add_layer(osm)
+
+countriesName = "ne_50m_admin_0_countries"
+
+schema = {
+    "name":"string",
+}
+
+centroidsLayer = HVectorLayer.new("centroids","Point","EPSG: 4326", schema)
+countriesLayer = HVectorLayer.open(gpkgPATH, countriesName)
+
+nonInCountryList = []
+nameIndex = countriesLayer.field_index("NAME")
+for country in countriesLayer.features():
+    countryGeom = country.geometry
+    name = country.attributes[nameIndex]
+    
+    centroid = countryGeom.centroid()
+    
+    centroidsLayer.add_feature(centroid, [name])
+    
+    if not centroid.intersects(countryGeom):
+        nonInCountryList.append(name)
+    
+simpleStyle = HMarker("circle", 10) + HLabel("name") + HHalo ()
+centroidsLayer.set_style(simpleStyle)
+HMap.add_layer(centroidsLayer)    # temporary layer
+
+print("Countries with centroids outside:")
+for c in nonInCountryList:
+    print(c)
+    
+
+
+#############
+
+ranges = [
+    [80000000, float('inf')],
+    [1000000, 80000000],
+    [float('-inf'),1000000]
+]
+
+styles = [
+    HFill("255, 0, 0, 70"),
+    HFill("0, 255, 0, 70"),
+    HFill("0, 0, 255, 70"),
+]
+    
+labelstyle = HLabel("POP_EST") + HHalo()
+
+countriesLayer.set_graduated_style("POP_EST", ranges, styles, labelstyle)
+
+HMap.add_layer(countriesLayer)
